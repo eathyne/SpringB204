eb01e9f32337e93776eb86ab5002c449f4b7d65c
Eathyn Edwards <eathynedwards@Earyns-MacBook-Air.local>
2019-12-01 13:41:03 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringB204Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringB204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringB204Application.class, args);
+    }
+
+}
